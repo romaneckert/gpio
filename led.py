d879adc3a88660013677353d89bfc0c468129799
Roman Eckert <mail@romaneckert.de>
2016-07-10 14:27:41 +0200

    update led.py

--- led.py
+++ led.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+
+ledPin = 11
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(LedPin,GPIO.OUT)
+GPIO.output(LEdPin,GPIO.HIGH)
+
+try:
+    while true:
+        print '...led on'
+        GPIO.output(LedPin, GPIO.LOW)
+        time.sleep(0.5)
+        print 'led off...'
+        GPIO.output(LedPin, GPIO.HIGH)
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    GPIO.output(LedPin, GPIO.HIGH)
+    GPIO.cleanup()
