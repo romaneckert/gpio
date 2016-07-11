00b4a4f0b2103c6908778cdc5a691679f0ae2a4f
Roman Eckert <mail@romaneckert.de>
2016-07-11 22:22:47 +0200

    add gpio image / play with leds

--- led.py
+++ led.py
@@ -3,25 +3,40 @@
 import RPi.GPIO as GPIO
 import time
 
-LedPin = 11
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(LedPin,GPIO.OUT)
-GPIO.output(LedPin,GPIO.HIGH)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(27,GPIO.OUT)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)
 
 try:
     while True:
 
         print '...start...'
-        GPIO.output(LedPin, GPIO.LOW)
-        time.sleep(2)
-        GPIO.output(LedPin, GPIO.HIGH)
-        time.sleep(0.5)
-        GPIO.output(LedPin, GPIO.LOW)
-        time.sleep(0.5)
-        GPIO.output(LedPin, GPIO.HIGH)
-        time.sleep(0.5)
+        GPIO.output(27, 1)
+        time.sleep(0.1)
+        GPIO.output(27, 0)
+        time.sleep(0.1)
+        GPIO.output(17, 1)
+        time.sleep(0.1)
+        GPIO.output(17, 0)
+        time.sleep(0.1)
+        GPIO.output(13, 1)
+        time.sleep(0.1)
+        GPIO.output(13, 0)
+        time.sleep(0.1)
+        GPIO.output(17, 1)
+        time.sleep(0.1)
+        GPIO.output(17, 0)
+        time.sleep(0.1)
+        # GPIO.output(LedPin, GPIO.HIGH)
+        # time.sleep(0.5)
+        # GPIO.output(LedPin, GPIO.LOW)
+        # time.sleep(0.5)
+        # GPIO.output(LedPin, GPIO.HIGH)
+        # time.sleep(0.5)
 
 except KeyboardInterrupt:
-    GPIO.output(LedPin, GPIO.HIGH)
+    GPIO.output(27, 0)
+    GPIO.output(17, 0)
+    GPIO.output(13, 0)
     GPIO.cleanup()
