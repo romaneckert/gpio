92ddf12718cdc13c1cf6f6d5be5c9a63cbb81b2e
Roman Eckert <mail@romaneckert.de>
2016-07-12 21:00:13 +0200

    implement loop with delta time

--- led_glowing.py
+++ led_glowing.py
@@ -3,21 +3,80 @@
 import RPi.GPIO as GPIO
 import time
 
+FPS = 60
+
+lastFrameTime = time.time()
+currentTime = time.time()
+deltaTime = 0
+sleepTime = 0
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
 
-p = GPIO.PWM(17, 1000)     # set Frequece to 1KHz
-p.start(0)
+p17 = GPIO.PWM(17, 1000)     # set Frequece to 1KHz
+p17.start(0)
+p17dc = 0
+p17dc_direction = 1
+
+p13 = GPIO.PWM(13, 1000)     # set Frequece to 1KHz
+p13.start(0)
+p13dc = 0
+p13dc_direction = 1
+
+def update():
+
+    global p17dc_direction
+    global p17dc
+
+    global p13dc_direction
+    global p13dc
+
+    p17dc += deltaTime * 100 * p17dc_direction
+
+    if p17dc < 0:
+        p17dc = 0
+        p17dc_direction *= -1
+
+    if p17dc > 100:
+        p17dc = 100
+        p17dc_direction *= -1
+
+    p17.ChangeDutyCycle(p17dc)
+
+    p13dc += deltaTime * 100 * p13dc_direction * 3
+
+    if p13dc < 0:
+        p13dc = 0
+        p13dc_direction *= -1
+
+    if p13dc > 100:
+        p13dc = 100
+        p13dc_direction *= -1
+
+    p13.ChangeDutyCycle(p13dc)
+
+    print "%s %s" % (p17dc, p17dc_direction)
+
 
 try:
     while True:
-        for dc in range(0, 101, 5):  # Increase duty cycle: 0~100
-            p.ChangeDutyCycle(dc)  # Change duty cycle
-            time.sleep(0.05)
-        for dc in range(100, -1, -5):  # Decrease duty cycle: 100~0
-            p.ChangeDutyCycle(dc)
-            time.sleep(0.05)
+
+        currentTime = time.time()
+        deltaTime = currentTime - lastFrameTime
+        lastFrameTime = currentTime
+
+        update()
+
+        sleepTime = 1.0/FPS - deltaTime
+        if sleepTime > 0:
+            time.sleep(sleepTime)
+
+        #p17.ChangeDutyCycle(deltaTime)
+
+
 
 except KeyboardInterrupt:
     GPIO.output(17, 0)
+    GPIO.output(13, 0)
     GPIO.cleanup()
