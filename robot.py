6ee1889446b19740e72b98352df77ffaa2ff140a
Roman Eckert <mail@romaneckert.de>
2016-07-17 22:09:02 +0200

    add robot py for servo

--- robot.py
+++ robot.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 from hawk.controller import Controller
+import RPi.GPIO as GPIO
 
 
 class Robot(Controller):
 
+    motor = None
+
     def __init__(self):
-        Controller.__init__(self)
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(17, GPIO.OUT)
+        self.motor = GPIO.PWM(17, 50)
+        self.motor.start(5)
+
+        self.activate()
 
     def update(self):
+
         print "delta: " + str(self.delta_time)
 
 
